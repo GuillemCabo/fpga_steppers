b1680733d7f2055b4b03059aa9e5a11ed9ef467d
guillem <cabo.guillem@gmail.com>
2017-05-17 17:38:32 +0200

    leds display nº of turns

--- encoder_led/steper_enc_v0.1.v
+++ encoder_led/steper_enc_v0.1.v
@@ -1,20 +1,22 @@
 `default_nettype none
-module driver_to_led (CLK,mo,ext_clk,LPORT,SW1,en,idir,dir,step);
+module driver_to_led #(parameter n_enc = 400 , n_steps = 800, mode = 2'b01) 
+(CLK,LPORT,mo,ext_clk,SW1,en,idir,dir,step);
     output  idir,dir,step;
-    output [7:0] LPORT; 
+    reg [10:0] r_enc;
     input [1:0] en;
     output  [1:0] mo;
+    output  [7:0] LPORT;
     input CLK,ext_clk,SW1;
         
 //step modes
-//mo [0] mo[1]
-// 0      0    full
-// 1      0    half
-// x      0    1/4
-// 0      1    1/8
+//mo [1] mo[0] mode steps
+// 0      0    full 400
+// 0      1    half 800
+// 0      x    1/4
+// 1      0    1/8
 // 1      1    1/16
-// x      1    1/32
-    assign mo = 2'b11;
+// 1      x    1/32
+    assign mo = mode;
 
     always @ (posedge SW1)
        dir=~dir;
@@ -27,13 +29,24 @@ module driver_to_led (CLK,mo,ext_clk,LPORT,SW1,en,idir,dir,step);
             if (en[0]==1)
                 begin
                 idir<=1;
-                LPORT<=LPORT+1;
+                    if (r_enc<n_enc)
+                        r_enc<=r_enc+1;
+                    else
+                        begin
+                           LPORT <= LPORT+1;
+                           r_enc <= 0;
+                        end
                 end
             else 
                 begin
-                LPORT<=LPORT-1;
                 idir<=0;
+                    if (r_enc>0)
+                        r_enc<=r_enc-1;
+                    else
+                        begin
+                            LPORT <= LPORT-1;
+                            r_enc <= n_enc;
+                        end
                 end
          end
-            
  endmodule
